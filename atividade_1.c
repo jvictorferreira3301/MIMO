3c960000ac72b1339d16dcecc6abd96e083aa871
Kauan Miranda <kkauanjjk@gmail.com>
2023-03-17 22:00:00 -0300

    Terceira Contribuição

--- atividade_1.c
+++ atividade_1.c
@@ -3,5 +3,6 @@
 int main(){
 	printf("João Victor Santos Brito Ferreira - 202207040028\n");
 	printf("Natalie Giovanna Pinto Da Cunha - 202206840039\n");
+	printf("Kauan Miranda Tavares - 2022068400033");
 return 0;
 }
