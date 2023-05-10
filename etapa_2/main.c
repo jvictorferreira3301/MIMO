600560830cc9637ffe2f48c3a7827d32a4e4c92a
João Victor Ferreira <121932120+jvictorferreira3301@users.noreply.github.com>
2023-05-09 22:52:09 -0300

    Update main.c

---
+++ etapa_2/main.c
@@ -0,0 +1,13 @@
+@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "matrizes.h"
+
+int main(void)
+{
+    printf("=============Equipe=============\n");
+	printf("Marco Antonio do Espirito Santo Maues Junior\n");
+	printf("Kauan Miranda Tavares\n ");
+	printf("João Victor Santos Brito Ferreira\n ");
+	teste_todos();
+}
