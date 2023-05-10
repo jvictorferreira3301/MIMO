8aaf0610d599ed6f2b8dd21c21872f02cc096554
João Victor Ferreira <121932120+jvictorferreira3301@users.noreply.github.com>
2023-05-09 22:56:07 -0300

    implementação doproduto interno e o respectivo teste

--- matrizes.h
+++ matrizes.h
@@ -1,19 +1,53 @@
-#ifndef MATRIZES_H
-#define MATRIZES_H
+@@ -0,0 +1,52 @@
+#ifndef _H_MYCOMPLEX
+#define _H_MYCOMPLEX
 
-typedef struct {
-    double real;
-    double imag;
-} Complexo;
+#include <stdlib.h>
 
-typedef struct {
-    int linhas;
-    int colunas;
-    Complexo **elementos;
-} MatrizComplexa;
+/*Estrutura das variáveis complexas*/
+typedef struct complexo {
+    float real;
+    float img;
+} complexo;
 
-MatrizComplexa cMatrizComplexa(int linhas, int colunas); //aloca��o din�mica
-void libMatrizComplexa(MatrizComplexa *matriz); // 
-MatrizComplexa somaMatrizesComplexas(MatrizComplexa matriz1, MatrizComplexa matriz2); // recebe duas matrizes como par�metro e retorna a soma delas
+complexo soma_complexo(complexo c1, complexo c2);
 
+/* Alocação de memoria para uma matriz complexa*/
+complexo** allocateComplexMatrix(int linhas, int colunas);
+
+/* Liberação de memória de uma matriz complexa*/
+void LiberarMatriz(complexo **mtx, int linhas);
+
+/* a função que faz a transposta da matriz*/
+complexo** transposta(complexo **c, int linhas, int colunas);
+
+/* Printa o complexo c */
+void printComplex(complexo c);
+
+/* Multiplica dois complexos */
+complexo multcomp(complexo c, complexo c1);
+
+/* Soma duas matrizes complexas */
+complexo** soma(complexo **c, complexo **c1, int linhas, int colunas);
+
+/* Subtrai duas matrizes complexas */
+complexo** subtracao(complexo **c, complexo **c2, int linhas, int colunas);
+
+/* Multiplica duas matrizes */
+complexo** produto_matricial(complexo **mtx_a, complexo **mtx_b, int linhas, int colunas);
+
+/* Calcula o produto interno entre dois vetores */
+complexo produto_interno(complexo **v1, complexo **v2, int linhas, int colunas);
+
+complexo** conjugada(complexo **mtx, int linhas, int colunas);
+
+complexo** hermetiana(complexo **mtx, int linhas, int colunas);
+
+void teste_conjugada(void);
+void teste_transposta(void);
+void teste_hermetiana(void);
+void teste_soma(void);
+void teste_subtracao(void);
+void teste_todos(void);
+void teste_produto_interno(void);
 #endif
