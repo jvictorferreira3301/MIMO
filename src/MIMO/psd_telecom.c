15487d95d719cb7a3962e4a7e0f7da72051648c9
Marco Maues <marcomauesjr@gmail.com>
2023-07-06 01:20:56 -0300

    Atualização para saida do nome dos arquivos finais e criação do ruido, com range especifico

--- src/MIMO/psd_telecom.c
+++ src/MIMO/psd_telecom.c
@@ -81,6 +81,19 @@ complexo ** tx_layer_mapper(complexo *v, int Nstream, long int numBytes){
     return mtx_stream;
 }
 
+complexo* rx_layer_demapper(complexo** mtx_stream, int Nstream, long int numBytes) {
+    complexo* v = (complexo*) malloc(numBytes * 4 * sizeof(complexo));
+    if (v == NULL) {
+        printf("Erro na alocação de memória.\n");
+        return NULL;
+    }
+
+    for (int i = 0; i < numBytes * 4; i++) {
+        v[i] = mtx_stream[i % Nstream][i / Nstream];
+    }
+
+    return v;
+}
 
 int* rx_qam_demapper(complexo * map, long int numBytes){
 
@@ -105,14 +118,14 @@ int* rx_qam_demapper(complexo * map, long int numBytes){
     }
     return vetor;
 }
-void rx_data_write(int *s, long int numBytes) {
-    FILE *out = fopen("saida", "wb");
+void rx_data_write(int* s, long int numBytes, char* fileName) {
+    FILE* out = fopen(fileName, "wb");
     if (out == NULL) {
-        printf("Erro ao abrir o arquivo para escrita.\n");
+        printf("Erro ao abrir o arquivo %s para escrita.\n", fileName);
         return;
+    } else {
+        printf("Arquivo %s criado com sucesso.\n", fileName);
     }
-    else
-        printf("Arquivo com a mensagem recebida criada com sucesso\n");
 
     for (int i = 0; i < numBytes; i++) {
         unsigned char byte = 0;
@@ -125,6 +138,7 @@ void rx_data_write(int *s, long int numBytes) {
 
     fclose(out);
 }
+
 
 float ** channel_gen(int Nr, int Nt, float minValue, float maxValue){
     float** H;
@@ -191,7 +205,7 @@ int main() {
     
     int Nr; // Número de antenas recpetoras
     int Nt; // Número de antenas transmissoras
-    int num_teste = 1; // Numero de testes necessarios
+    int num_teste = 16; // Numero de testes necessarios
     
     for(int teste = 1; teste<=num_teste; teste++){
         
@@ -202,7 +216,7 @@ int main() {
             Nt = 4;
             
         }
-        else if (teste>4 && teste>=8){
+        else if (teste>4 && teste<=8){
             Nr = 8;
             Nt = 8;
         }
@@ -210,12 +224,13 @@ int main() {
             Nr = 8;
             Nt = 32;
         }
-        else{
+        else if (teste>12 && teste <=16){
             Nr = 16;
             Nt = 32;
         }
-        printf("Numero de antenas recpetoras: %d // Numero de antenas Transmissoras: %d\n",Nr,Nt);
+
         int Nstream = Nr;
+        printf("Numero de antenas recpetoras: %d // Numero de antenas Transmissoras: %d // Numero de fluxos: %d\n",Nr,Nt,Nstream);
         // Leitura do arquivo
         printf("Realizando leitura do Arquivo...\n");
         int *s=tx_data_read(fp,numBytes);
@@ -229,35 +244,43 @@ int main() {
         complexo *map = tx_qam_mapper(d,numBytes);
 
         // Criação do Canal H com range entre -1 e 1
-        printf("Criação do Canal de transferencia de Dados:\n");
-        /*float ** H = channel_gen(Nr,Nt,-1,1);
-        for(int l = 0; l<Nr;l++){
-            for(int c = 0; c<Nt;c++){
-                printf("%f ",H[l][c]);
-            }
-            printf("\n");
-        }*/
+        printf("Criação do Canal de transferencia de Dados...\n");
+        float ** H = channel_gen(Nr,Nt,-1,1);
+        int ruido;
+        if(teste == 1 || teste == 5 || teste == 9 || teste == 13){
+            float **Rd=channel_gen(Nr,Nt,-1,1);
+            ruido = 1;
+        }
+        else if(teste == 2 || teste == 6 || teste == 10 || teste == 14){
+            float **Rd=channel_gen(Nr,Nt,-0.5,0.5);
+            ruido = 2;
+        }
+        else if(teste == 3 || teste == 7 || teste == 11 || teste == 15){
+            float **Rd=channel_gen(Nr,Nt,-0.1,0.1);
+            ruido = 3;
+        }
+        else if(teste == 4 || teste == 8 || teste == 12 || teste == 16){
+            float **Rd=channel_gen(Nr,Nt,-0.01,0.01);
+            ruido = 4;
+        }
 
         //Transformando o vetor complexo do mapaeamento para uma matriz complexa
         complexo **mtx= tx_layer_mapper(map, Nstream, numBytes);
-        
-        /*for(int l=0;l<Nstream;l++){
-            for(int c=0; c<numBytes*2;c++){
-                printf("%+f %+f",mtx[l][c].real,mtx[l][c].img);
-            }
-            printf("\n");
-        }*/
+
+        complexo *v=rx_layer_demapper(mtx,Nstream,numBytes);
 
         // Desmapeamento dos bits do arquivo
         printf("Realizando Desmapeamento dos Bits do Arquivo...\n");
-        int *a=rx_qam_demapper(map,numBytes);
+        int *a=rx_qam_demapper(v,numBytes);
 
         // Leitura Final dos Dados
-        printf("Salvando arquivo com a mensagem enviada no arquivo test_Nr%d_Nt%d\n",Nr,Nt);
-        rx_data_write(d,numBytes);
+        printf("Salvando arquivo com a mensagem enviada no arquivo test_Nr%d_Nt%d_Rd%d\n",Nr,Nt,ruido);
+        
+        char fileName[100];
+        sprintf(fileName, "Teste_Nr%d_Nt%d_Rd%d", Nr,Nt,ruido); // Formata o nome do arquivo com base no valor de i
+        rx_data_write(s, numBytes, fileName);
     }
 
-    
     fclose(fp);
     return 0;
 }
