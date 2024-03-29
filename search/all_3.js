e0b756943f539c52559a40ae0fb571e76d1c12c4
jvictorferreira3301 <jvictorferreira3301@users.noreply.github.com>
2024-03-29 15:17:08 +0000

    deploy: fb3b5686a25c3de679bf62a8cd3cc42c1769bfb8

---
+++ search/all_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['hermitiana_8',['hermitiana',['../matrizes_8c.html#a4c238ca1a9358c2ed01b2d855575d528',1,'matrizes.c']]]
+];
---
+++ search/files_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['matrizes_2ec_46',['matrizes.c',['../matrizes_8c.html',1,'']]]
+];
